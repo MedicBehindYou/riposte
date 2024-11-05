3dcc7f24c0b7f6b59042ac4d161d061be14a1ac9
MedicBehindYou <byy0url3ave@gmail.com>
2024-11-05 15:39:56 -0600

    moved Recipients inside script files

--- app/action.py
+++ app/action.py
@@ -3,8 +3,6 @@ from .misc import send_email  # noqa: F401
 from fastapi import HTTPException  # noqa: F401
 import os
 
-recipients = ['esmith@impactnetworking.com', 'sredman@impactnetworking.com']
-
 def execute(token, riposteSubmit, db):
     path = os.path.join(os.path.dirname(__file__), 'config', 'scripts', token)
     with open(path) as f:
